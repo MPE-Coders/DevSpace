bd416e037ddf1f148d809f9be2148706960dd58b
XackiGiFF <m.girsanoff@mc-mpe.ru>
2024-09-10 09:23:20 +0300

    Обновлены субмодули

--- merge.sh
+++ merge.sh
@@ -1,50 +1,32 @@
 #!/bin/bash
 
-
-
 # Перейти в каталог субмодуля
 
 cd ./plugins/
 
-
-
 # Добавить изменения и сделать коммит в субмодуле
 
 git add .
-
 git commit -m "Обновлены субмодули"
-
 git pull origin main
-
-
 
 # Пуш изменений в субмодуле
 
 git push origin main
 
-
-
 # Вернуться в основной проект
 
 cd ..
-
-
 
 # Обновить субмодули
 
 git submodule update --remote --merge
 
-
-
 # Добавить изменения в основной проект и сделать коммит
 
 git add .
-
 git commit -m "Обновлены субмодули"
-
 git pull origin master
-
-
 
 # Пуш изменений в основной проект
 
